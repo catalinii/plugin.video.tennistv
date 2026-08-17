33fc16bf4ef248fd77de5c6ad4ee301b7e0d277c
Catalin Toda <catalinii@yahoo.com>
2026-08-17 04:16:15 -0700

    Update pack.sh exclusions for clean Kodi zip structure

--- pack.sh
+++ pack.sh
@@ -7,12 +7,15 @@ ZIP_NAME="${ADDON_DIR_NAME}.zip"
 
 cd "$SCRIPT_DIR/.."
 
+rm -f "$SCRIPT_DIR/$ZIP_NAME"
+
 zip -r "$SCRIPT_DIR/$ZIP_NAME" "$ADDON_DIR_NAME" \
     -x "$ADDON_DIR_NAME/.git*" \
     -x "$ADDON_DIR_NAME/pack.sh" \
     -x "$ADDON_DIR_NAME/$ZIP_NAME" \
     -x "$ADDON_DIR_NAME/*.pyc" \
-    -x "$ADDON_DIR_NAME/__pycache__/*" \
+    -x "*__pycache__*" \
+    -x "*.DS_Store*" \
     -x "$ADDON_DIR_NAME/.vscode/*" \
     -x "$ADDON_DIR_NAME/.idea/*"
 
